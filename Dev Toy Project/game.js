9a60b390e7f64595be0ba951916a9313dcf71565
Korutesu9080 <kingcrimson9080@gmail.com>
2026-01-30 08:55:11 -0800

    Update game.js

--- Dev Toy Project/game.js
+++ Dev Toy Project/game.js
@@ -1,170 +1,151 @@
 /*
-game.js for Perlenspiel 3.3.x
-Last revision: 2022-03-15 (BM)
-Perlenspiel is a scheme by Professor Moriarty (bmoriarty@wpi.edu).
-This version of Perlenspiel (3.3.x) is hosted at <https://ps3.perlenspiel.net>
-Perlenspiel is Copyright © 2009-22 Brian Moriarty.
-This file is part of the standard Perlenspiel 3.3.x devkit distribution.
-Perlenspiel is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as published
-by the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-Perlenspiel is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU Lesser General Public License for more details.
-You may have received a copy of the GNU Lesser General Public License
-along with the Perlenspiel devkit. If not, see <http://www.gnu.org/licenses/>.
+Rainbow Splash Stim Toy – Perlenspiel 3.3.x
+By Jason Cortez
 */
-/*
-This JavaScript file is a template for creating new Perlenspiel 3.3.x games.
-Any unused event-handling function templates can be safely deleted.
-Refer to the tutorials and documentation at <https://ps3.perlenspiel.net> for details.
-*/
-/*
-The following comment lines are for JSHint <https://jshint.com>, a tool for monitoring code quality.
-You may find them useful if your development environment is configured to support JSHint.
-If you don't use JSHint (or are using it with a configuration file), you can safely delete these two lines.
-*/
-/* jshint browser : true, devel : true, esversion : 6, freeze : true */
-/* globals PS : true */
-"use strict"; // Do NOT remove this directive!
 
-// Flatten the original 3x3 color array into a 1D list for easy cycling
-const COLOR_LIST = [
-	PS.COLOR_GREEN, PS.COLOR_PURPLE, PS.COLOR_ORANGE,
-	PS.COLOR_RED, PS.COLOR_BLUE, PS.COLOR_YELLOW,
-	PS.COLOR_CYAN, PS.COLOR_GRAY, PS.COLOR_BLACK
+"use strict";
+
+/* =======================
+   COLOR PALETTES
+======================= */
+
+const PALETTES = [
+	// Classic rainbow
+	[
+		PS.COLOR_RED,
+		PS.COLOR_ORANGE,
+		PS.COLOR_YELLOW,
+		PS.COLOR_GREEN,
+		PS.COLOR_CYAN,
+		PS.COLOR_BLUE,
+		PS.COLOR_PURPLE
+	],
+
+	// Pastel
+	[
+		0xFFB3BA,
+		0xFFDFBA,
+		0xFFFFBA,
+		0xBAFFC9,
+		0xBAE1FF
+	],
+
+	// Neon
+	[
+		0xFF005D,
+		0xFFFB00,
+		0x00FF9C,
+		0x00B3FF,
+		0x9D00FF
+	]
 ];
 
-// Game state variables
-let currentIndex = 0;      // Current position in COLOR_LIST
-let timerId = 0;           // ID of the active timer (0 = no timer)
-let centerX = 0;           // X position of the last clicked bead (spread center)
-let centerY = 0;           // Y position of the last clicked bead (spread center)
-let leftFront = 0;         // Current front of leftward spread
-let rightFront = 0;        // Current front of rightward spread
-let upFront = 0;           // Current front of upward spread
-let downFront = 0;         // Current front of downward spread
+let paletteIndex = 0;
+let COLOR_LIST = PALETTES[paletteIndex];
 
-// The spreading function (called repeatedly by the timer)
+/* =======================
+   GAME STATE
+======================= */
+
+let currentIndex = 0;
+let timerId = 0;
+
+let centerX = 0;
+let centerY = 0;
+let radius = 1;
+
+/* =======================
+   SOUNDS
+======================= */
+
+let clickSound = "fx_click";
+let spreadSound = "fx_pop";
+
+/* =======================
+   SPREAD LOGIC
+======================= */
+
 const spreadTick = function () {
+	let color = COLOR_LIST[currentIndex % COLOR_LIST.length];
+	currentIndex++;
+
 	let changed = false;
-	let color;
 
-	if (leftFront >= 0) {
-		color = COLOR_LIST[currentIndex % COLOR_LIST.length];
-		PS.color(leftFront, centerY, color);
-		currentIndex += 1;
-		leftFront -= 1;
+	if (centerX - radius >= 0) {
+		PS.color(centerX - radius, centerY, color);
+		changed = true;
+	}
+	if (centerX + radius < 24) {
+		PS.color(centerX + radius, centerY, color);
+		changed = true;
+	}
+	if (centerY - radius >= 0) {
+		PS.color(centerX, centerY - radius, color);
+		changed = true;
+	}
+	if (centerY + radius < 24) {
+		PS.color(centerX, centerY + radius, color);
 		changed = true;
 	}
 
-	if (rightFront < 8) {
-		color = COLOR_LIST[currentIndex % COLOR_LIST.length];
-		PS.color(rightFront, centerY, color);
-		currentIndex += 1;
-		rightFront += 1;
-		changed = true;
-	}
-
-	if (upFront >= 0) {
-		color = COLOR_LIST[currentIndex % COLOR_LIST.length];
-		PS.color(centerX, upFront, color);
-		currentIndex += 1;
-		upFront -= 1;
-		changed = true;
-	}
-
-	if (downFront < 8) {
-		color = COLOR_LIST[currentIndex % COLOR_LIST.length];
-		PS.color(centerX, downFront, color);
-		currentIndex += 1;
-		downFront += 1;
-		changed = true;
-	}
-
-	if (!changed) {
+	if (changed) {
+		PS.audioPlay(spreadSound, { volume: 0.2 });
+		radius++;
+	} else {
 		PS.timerStop(timerId);
 		timerId = 0;
 	}
 };
 
-/*
-PS.init( system, options )
-Called once after engine is initialized but before event-polling begins.
-*/
-PS.init = function (system, options) {
-	PS.gridSize(8, 8);
+/* =======================
+   INIT
+======================= */
 
-	// Start with a neutral gray grid so all colors are visible
-	PS.color(PS.ALL, PS.ALL, PS.COLOR_GRAY);
+PS.init = function () {
+	PS.gridSize(24, 24);
 
-	// Remove borders for a cleaner look
+	// Dark background for contrast
+	PS.color(PS.ALL, PS.ALL, 0x202020);
 	PS.border(PS.ALL, PS.ALL, 0);
 
-	PS.statusText("Trail Game");
+	PS.statusText("Click to splash | 1–3 palettes");
 
-	// Optional welcome message
-	// PS.statusText( "Click any bead to spread rainbow trails!" );
+	PS.audioLoad(clickSound);
+	PS.audioLoad(spreadSound);
 };
 
-/*
-PS.touch ( x, y, data, options )
-Called when a bead is clicked/touched.
-This starts a new rainbow spread centered on the clicked bead.
-*/
-PS.touch = function (x, y, data, options) {
-	// Reset color cycle for each new spread (starts with green in the center)
+/* =======================
+   INPUT
+======================= */
+
+PS.touch = function (x, y) {
 	currentIndex = 0;
+	radius = 1;
 
-	// Color the center bead first
-	PS.color(x, y, COLOR_LIST[0]);
-	currentIndex = 1;
-
-	// Set up the spread fronts
 	centerX = x;
 	centerY = y;
-	leftFront = x - 1;
-	rightFront = x + 1;
-	upFront = y - 1;
-	downFront = y + 1;
 
-	// Stop any previous spread
+	PS.color(x, y, COLOR_LIST[0]);
+	PS.audioPlay(clickSound, { volume: 0.3 });
+
 	if (timerId) {
 		PS.timerStop(timerId);
 	}
 
-	// Start a new spread timer (12 ticks/second = fairly quick spread)
-	timerId = PS.timerStart(12, spreadTick);
+	timerId = PS.timerStart(8, spreadTick);
 };
 
-/* The remaining event handlers are left empty (you can delete them if you want) */
+PS.keyDown = function (key) {
+	if (key === 49) paletteIndex = 0; // 1
+	if (key === 50) paletteIndex = 1; // 2
+	if (key === 51) paletteIndex = 2; // 3
 
-PS.release = function (x, y, data, options) {
-	// Unused
+	COLOR_LIST = PALETTES[paletteIndex];
 };
 
-PS.enter = function (x, y, data, options) {
-	// Unused
-};
+/* =======================
+   UNUSED EVENTS
+======================= */
 
-PS.exit = function (x, y, data, options) {
-	// Unused
-};
-
-PS.exitGrid = function (options) {
-	// Unused
-};
-
-PS.keyDown = function (key, shift, ctrl, options) {
-	// Unused
-};
-
-PS.keyUp = function (key, shift, ctrl, options) {
-	// Unused
-};
-
-PS.input = function (sensors, options) {
-	// Unused
-};
+PS.release = PS.enter = PS.exit = PS.exitGrid =
+PS.keyUp = PS.input = function () {};
